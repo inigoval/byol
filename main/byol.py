b826e7d003b75407335fa394c1b3ba349d376320
inigoval <inigoval@gmail.com>
2022-04-07 11:10:32 +0100

    Moved _get_backbone and _get_net to networks.models

--- main/byol.py
+++ main/byol.py
@@ -16,6 +16,7 @@ from lightly.models.utils import update_momentum
 from pytorch_lightning.callbacks import Callback
 
 from evaluation import knn_predict
+from networks.models import _get_backbone
 
 
 class byol_old_lightly(pl.LightningModule):
@@ -25,7 +26,7 @@ class byol_old_lightly(pl.LightningModule):
         self.save_hyperparameters()  # save hyperparameters for easy inference
         self.config = config
 
-        self.backbone = self._get_backbone()
+        self.backbone = _get_backbone(config)
 
         # create a byol model based on ResNet
         features = self.config["model"]["features"]
@@ -153,7 +154,7 @@ class byol_old_lightly(pl.LightningModule):
             return opt
 
     def _get_backbone(self):
-        net = self._get_net()
+        net = _get_net()
         c_out = list(net.children())[-1].in_features
 
         net = torch.nn.Sequential(*list(net.children())[:-1])
@@ -193,7 +194,7 @@ class BYOL(pl.LightningModule):
         self.save_hyperparameters()  # save hyperparameters for easy inference
         self.config = config
 
-        self.backbone = self._get_backbone()
+        self.backbone = _get_backbone(config)
 
         # create a byol model based on ResNet
         features = self.config["model"]["features"]
@@ -291,40 +292,6 @@ class BYOL(pl.LightningModule):
             self.best_acc = acc
         self.log("val/kNN_acc", acc)
         self.log("val/max_kNN_acc", self.best_acc)
-
-    def _get_backbone(self):
-        net = self._get_net()
-        c_out = list(net.children())[-1].in_features
-
-        net = torch.nn.Sequential(*list(net.children())[:-1])
-
-        # Change first layer for color channels B/W images
-        n_c = self.config["data"]["color_channels"]
-        if n_c != 3:
-            net[0] = nn.Conv2d(n_c, 64, 7, 2, 3)
-
-        features = self.config["model"]["features"]
-        backbone = nn.Sequential(
-            *list(net.children())[:-1],
-            nn.Conv2d(c_out, features, 1),
-            nn.AdaptiveAvgPool2d(1),
-        )
-
-        return backbone
-
-    def _get_net(self):
-        networks = {
-            "resnet18": M.resnet18,
-            "resnet34": M.resnet34,
-            "resnet50": M.resnet50,
-            "resnet101": M.resnet101,
-            "resnet152": M.resnet152,
-            "wide_resnet50_2": M.wide_resnet50_2,
-            "wide_resnet101_2": M.wide_resnet101_2,
-            "efficientnetb7": M.efficientnet_b7,
-        }
-
-        return networks[self.config["model"]["arch"]]()
 
 
 class Update_M(Callback):
--- main/networks/models.py
+++ main/networks/models.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
 import torchvision.models as models
+import torchvision.models as M
 
 from networks.layers import conv_block, convT_block, linear_block, UPSoftmax
 
@@ -25,94 +26,6 @@ class MLPHead(nn.Module):
         return self.net(x)
 
 
-class ResNet18(torch.nn.Module):
-    def __init__(self, *args, **kwargs):
-        super(ResNet18, self).__init__()
-        resnet = models.resnet18(pretrained=False)
-
-        self.encoder = torch.nn.Sequential(*list(resnet.children())[:-1])
-
-        # Change first layer for color channels B/W images
-        n_c = kwargs["data"]["color_channels"]
-        if n_c != 3:
-            self.encoder[0] = nn.Conv2d(n_c, 64, 7, 2, 3)
-
-        features = kwargs["model"]["features"]
-        if features:
-            c_out = list(resnet.children())[-1].in_features
-            self.encoder = nn.Sequential(
-                self.encoder,
-                nn.Conv2d(c_out, features, 1),
-                nn.AdaptiveAvgPool2d(1),
-            )
-
-        # Add projection layer
-        self.projection = MLPHead(
-            in_channels=resnet.fc.in_features, **kwargs["projection_head"]
-        )
-
-    def forward(self, x):
-        h = self.encoder(x)
-        h = h.view(h.shape[0], h.shape[1])
-        return self.projection(h)
-
-    def encode(self, x):
-        h = self.encoder(x)
-        h = h.view(h.shape[0], h.shape[1])
-        return h
-
-
-class ResNet50(torch.nn.Module):
-    def __init__(self, *args, **kwargs):
-        super(ResNet50, self).__init__()
-        resnet = models.resnet50(pretrained=False)
-
-        self.encoder = torch.nn.Sequential(*list(resnet.children())[:-1])
-
-        # Change first layer for 1 channel B/W images
-        self.encoder[0] = nn.Conv2d(1, 64, 7, 2, 3)
-
-        c_out = list(resnet.children())[-1].in_features
-        features = kwargs["model"]["features"]
-
-        self.encoder = nn.Sequential(
-            self.encoder,
-            #            nn.Conv2d(c_out, features, 1),
-            #             nn.AdaptiveAvgPool2d(1),
-        )
-
-        # Add projection layer
-        self.projection = MLPHead(
-            in_channels=resnet.fc.in_features, **kwargs["projection_head"]
-        )
-
-    def forward(self, x):
-        h = self.encoder(x)
-        h = h.view(h.shape[0], h.shape[1])
-        return self.projection(h)
-
-
-class WideResNet50_2(torch.nn.Module):
-    def __init__(self, *args, **kwargs):
-        super(WideResNet50_2, self).__init__()
-        resnet = models.wide_resnet50_2(pretrained=False)
-
-        self.encoder = torch.nn.Sequential(*list(resnet.children())[:-1])
-
-        # Change first layer for 1 channel B/W images
-        self.encoder[0] = nn.Conv2d(1, 64, 7, 2, 3)
-
-        # Add projection layer
-        self.projection = MLPHead(
-            in_channels=resnet.fc.in_features, **kwargs["projection_head"]
-        )
-
-    def forward(self, x):
-        h = self.encoder(x)
-        h = h.view(h.shape[0], h.shape[1])
-        return self.projection(h)
-
-
 class LogisticRegression(torch.nn.Module):
     def __init__(self, input_dim, output_dim):
         super(LogisticRegression, self).__init__()
@@ -120,3 +33,39 @@ class LogisticRegression(torch.nn.Module):
 
     def forward(self, x):
         return self.linear(x)
+
+
+def _get_backbone(config):
+    net = _get_net(config)
+    c_out = list(net.children())[-1].in_features
+
+    net = torch.nn.Sequential(*list(net.children())[:-1])
+
+    # Change first layer for color channels B/W images
+    n_c = config["data"]["color_channels"]
+    if n_c != 3:
+        net[0] = nn.Conv2d(n_c, 64, 7, 2, 3)
+
+    features = config["model"]["features"]
+    backbone = nn.Sequential(
+        *list(net.children())[:-1],
+        nn.Conv2d(c_out, features, 1),
+        nn.AdaptiveAvgPool2d(1),
+    )
+
+    return backbone
+
+
+def _get_net(config):
+    networks = {
+        "resnet18": M.resnet18,
+        "resnet34": M.resnet34,
+        "resnet50": M.resnet50,
+        "resnet101": M.resnet101,
+        "resnet152": M.resnet152,
+        "wide_resnet50_2": M.wide_resnet50_2,
+        "wide_resnet101_2": M.wide_resnet101_2,
+        "efficientnetb7": M.efficientnet_b7,
+    }
+
+    return networks[config["model"]["arch"]]()
