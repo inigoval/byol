cffe457161dc752b8f5e222c39a472fc1102ec46
Mike Walmsley <walmsleymk1@gmail.com>
2022-04-26 14:53:43 +0100

    avoiding modifying global.yml, gzmnist runs okay

--- byol_main/train.py
+++ byol_main/train.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         auto_insert_metric_name=False,
         verbose=True,
         dirpath="wandb/",
-        filename="full_124px_amp",
+        filename="{train/loss:.3f}",
         save_weights_only=True,
     )
 
