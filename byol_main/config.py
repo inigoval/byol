85adb585f6a187160d493a8821dc35d2d171506c
Inigo Val Slijepcevic <inigoval@gmail.com>
2022-05-09 17:34:47 +0100

    Removed deprecated function
    update_config removed

--- byol_main/config.py
+++ byol_main/config.py
@@ -37,38 +37,3 @@ def load_config():
 
 def update_config(config):
     """Update config with values requiring initialisation of config"""
-
-    # Learning rate scaling from BYOL
-    # config["lr"] = config["lr"] * config["batch_size"] / 256
-
-    # Adjust parameters for different data-sets
-    if config["dataset"] == "imagenette":
-        config["data"]["color_channels"] = 3
-        config["data"]["classes"] = 10
-        config["data"]["input_height"] = 128
-        config["center_crop_size"] = 128
-
-    if config["dataset"] == "rgz":
-        config["data"]["color_channels"] = 1
-        config["data"]["classes"] = 2
-        config["data"]["input_height"] = config["center_crop_size"]
-        config["data"]["rotate"] = True
-
-    # Adjust parameters for different data-sets
-    if config["dataset"] == "stl10":
-        config["data"]["color_channels"] = 3
-        config["data"]["classes"] = 10
-        config["data"]["input_height"] = 96
-        config["data"]["center_crop_size"] = 128
-
-    if config["dataset"] == "cifar10":
-        config["data"]["color_channels"] = 3
-        config["data"]["classes"] = 10
-        config["data"]["input_height"] = 32
-        config["data"]["center_crop_size"] = 32
-
-    if config["dataset"] == "gzmnist":
-        config["data"]["color_channels"] = 3
-        config["data"]["classes"] = 4
-        config["data"]["input_height"] = 64
-        config["data"]["rotate"] = True
