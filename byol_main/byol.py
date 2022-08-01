c809570f3d377f8727eaf15953cb7b97c4d81edc
Inigo Val Slijepcevic <inigoval@gmail.com>
2022-08-01 17:34:22 +0100

    Format BYOL_Supervised

--- byol_main/byol.py
+++ byol_main/byol.py
@@ -124,7 +124,6 @@ class Update_M(Callback):
 
 
 class BYOL_Supervised(BYOL):
-
     def __init__(self, config):
         super().__init__(config)
         # re-use the projection head pattern
@@ -132,41 +131,38 @@ class BYOL_Supervised(BYOL):
         supervised_in_features = self.config["model"]["features"]
 
         # new version - built on top of the projection head (but not prediction head)
-        # will use  the projection head output dim as feature dim 
+        # will use  the projection head output dim as feature dim
         # supervised_in_features = self.config['projection_head']['out']
 
         supervised_head_params = self.config["supervised_head"]
 
-        if self.config['supervised_loss_weight'] > 1e5:
-            logging.warning('Debug mode - using only supervised head loss and IGNORING contrastive loss')
+        if self.config["supervised_loss_weight"] > 1e5:
+            logging.warning("Debug mode - using only supervised head loss and IGNORING contrastive loss")
 
-        if supervised_head_params['training_mode'] == 'classification':
-            num_classes = config['data']['classes']
-            logging.info('Adding supervised head in classification mode, {} classes'.format(num_classes))
+        if supervised_head_params["training_mode"] == "classification":
+            num_classes = config["data"]["classes"]
+            logging.info("Adding supervised head in classification mode, {} classes".format(num_classes))
             # remember this has batch-norm
             self.supervised_head = nn.Sequential(
-                BYOLProjectionHead(supervised_in_features, supervised_head_params["hidden"], num_classes),
-                torch.nn.Softmax(dim=-1)
+                BYOLProjectionHead(
+                    supervised_in_features, supervised_head_params["hidden"], num_classes
+                ),
+                torch.nn.Softmax(dim=-1),
             )
             # ignore targets with value (aka class index) of -1
             self.supervised_loss_func = torch.nn.CrossEntropyLoss(ignore_index=-1)
 
-        elif supervised_head_params['training_mode'] == 'dirichlet':
-            num_outputs = supervised_head_params['out']
-            logging.info('Adding supervised head in dirichlet mode, {} outputs'.format(num_outputs))
-
-           
+        elif supervised_head_params["training_mode"] == "dirichlet":
+            num_outputs = supervised_head_params["out"]
+            logging.info("Adding supervised head in dirichlet mode, {} outputs".format(num_outputs))
 
             self.supervised_head = nn.Sequential(
-
                 # this prediction head is not quite the same as zoobot - has batchnorm, does not have dropout
                 # BYOLProjectionHead(supervised_in_features, supervised_head_params["hidden"], num_outputs),
                 # efficientnet_custom.ScaledSigmoid()   # sigmoid from 1 to 100
-
                 # this is exactly as with zoobot
                 custom_layers.PermaDropout(0.2),
-                efficientnet_custom.custom_top_dirichlet(supervised_in_features, num_outputs)
-
+                efficientnet_custom.custom_top_dirichlet(supervised_in_features, num_outputs),
             )
             # my losses. code uses the wrong input convention (torch does preds, labels, but I did labels, preds) - adjust with lambda
             # sum is over questions as losses.multiquestion_loss gives loss like (batch, neg_log_prob_per_question)
@@ -174,14 +170,16 @@ class BYOL_Supervised(BYOL):
 
                 if nan_safe:
                     if torch.any(torch.isnan(preds)):
-                        logging.warning('Found nan concentration predictions - skipping batch')
+                        logging.warning("Found nan concentration predictions - skipping batch")
                         return torch.zeros_like(preds).sum()  # using "like" for cuda placement
-                    elif torch.any(preds <= 0.):
-                        logging.warning('Found concentration predictions below 0 - skipping batch')
+                    elif torch.any(preds <= 0.0):
+                        logging.warning("Found concentration predictions below 0 - skipping batch")
                         return torch.zeros_like(preds).sum()  # using "like" for cuda placement
 
-                dirichlet_loss = losses.calculate_multiquestion_loss(labels, preds, supervised_head_params['question_index_groups'])
-                
+                dirichlet_loss = losses.calculate_multiquestion_loss(
+                    labels, preds, supervised_head_params["question_index_groups"]
+                )
+
                 # divide by labels.shape[1] to avoid a factor from num questions with sum/mean. Doesn't matter, just be consistent with zoobot
                 num_questions = labels.shape[1]
                 # divide by num labels > 0, to avoid reducing the mean with unlabelled data for which the loss is 0
@@ -192,13 +190,16 @@ class BYOL_Supervised(BYOL):
                 # currently too small by factor of num_unlabelled/num_labelled
                 # aka  loss * num_unlabelled/num_labelled  = goal loss
                 num_unlabelled_galaxies = len(labels)
-                empirical_factor = num_unlabelled_galaxies/num_labelled_galaxies
+                empirical_factor = num_unlabelled_galaxies / num_labelled_galaxies
 
-                return empirical_factor * torch.sum(dirichlet_loss)/(num_labelled_galaxies * num_questions)  # over both (batch, question)
+                return (
+                    empirical_factor
+                    * torch.sum(dirichlet_loss)
+                    / (num_labelled_galaxies * num_questions)
+                )  # over both (batch, question)
                 # p of (N=0, k=0) = 1 -> neg log p = 0 -> no effect on sum, but will reduce mean. Only absolute value though, not gradients per se if normalised
 
             self.supervised_loss_func = dirichlet_loss_aggregated_to_scalar
-
 
     def configure_optimizers(self):
         params = (
@@ -211,7 +212,7 @@ class BYOL_Supervised(BYOL):
         # TODO could use dif optimiser for sup head with dif learning rate, if desired
         return _optimizer(params, self.config)
 
-# I'm splitting self.project into two steps, self.represent then self.project, so I can use the rep without recalculating
+    # I'm splitting self.project into two steps, self.represent then self.project, so I can use the rep without recalculating
 
     def represent(self, x):
         # representation
@@ -224,7 +225,6 @@ class BYOL_Supervised(BYOL):
         # prediction (of proj of target network)
         p = self.prediction_head(z)
         return p
-
 
     def training_step(self, batch, batch_idx):
 
@@ -243,9 +243,9 @@ class BYOL_Supervised(BYOL):
         # keep same name for wandb comparison
         # print('supervised vs contrastive: ', supervised_loss, contrastive_loss)
         self.log("train/contrastive_loss", contrastive_loss, on_step=log_on_step, on_epoch=True)
-        self.log("train/supervised_loss", supervised_loss, on_step=log_on_step, on_epoch=True) 
+        self.log("train/supervised_loss", supervised_loss, on_step=log_on_step, on_epoch=True)
 
-        supervised_loss_weight = self.config['supervised_loss_weight']
+        supervised_loss_weight = self.config["supervised_loss_weight"]
         if supervised_loss_weight > 1e5:
             # debug mode - ignore contrastive entirely
             loss = supervised_loss
@@ -255,14 +255,20 @@ class BYOL_Supervised(BYOL):
         else:
             # normalise supervised loss by contrastive loss
             # will have the gradients from supervised loss, but scaled to by similar to contrastive loss
-            supervised_normalising_constant = torch.abs(contrastive_loss.detach()) / supervised_loss.detach()
-            loss = contrastive_loss + self.config['supervised_loss_weight'] * supervised_normalising_constant * supervised_loss  
-    
+            supervised_normalising_constant = (
+                torch.abs(contrastive_loss.detach()) / supervised_loss.detach()
+            )
+            loss = (
+                contrastive_loss
+                + self.config["supervised_loss_weight"]
+                * supervised_normalising_constant
+                * supervised_loss
+            )
+
         # total weighted loss, used for checkpoint monitoring
         # TODO might be better to use val/supervised_loss when available
-        self.log("train/loss", loss, on_step=log_on_step, on_epoch=True)  
+        self.log("train/loss", loss, on_step=log_on_step, on_epoch=True)
         return loss
-
 
     def calculate_losses(self, batch):
         # used in both train and val, so must have no side-effects on model(s)
@@ -275,7 +281,7 @@ class BYOL_Supervised(BYOL):
 
         y0 = self.represent(x0)  # supervised head uses y0
         p0 = self.project(y0)
-        
+
         y1 = self.represent(x1)  # y1 not used except below
         p1 = self.project(y1)  # but re-using same funcs
 
@@ -289,11 +295,6 @@ class BYOL_Supervised(BYOL):
         supervised_head_out = self.supervised_head(y0)
         # supervised_head_out = self.supervised_head(p0)
 
-        supervised_loss = self.supervised_loss_func(supervised_head_out, labels)  
+        supervised_loss = self.supervised_loss_func(supervised_head_out, labels)
 
         return contrastive_loss, supervised_loss
-    
-
-
-
-
